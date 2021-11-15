ebd14a242a17212a7d672687888af727a9d728d6
Adrien de Decker <adriendedecker@yahoo.fr>
2021-11-15 11:05:48 +0100

    Update accueil.php

--- views/accueil.php
+++ views/accueil.php
@@ -2,4 +2,5 @@
 			<h2>Accueil</h2>
 			<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
 			<p>Jean-Luc Collinet.</p>
+			<p>J'apporte des modification afin de voir si la pipeline fonctionne correctement</p>
 		</section>
